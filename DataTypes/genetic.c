30db4b93f440a2081016d1dec0e9d0dcd56df0d4
Jim Paton <jpaton2@gmail.com>
2010-10-24 10:15:46 -0500

    * changed back to old swap since it has better performance
    Signed-off-by: Jim Paton <jpaton2@gmail.com>

--- DataTypes/genetic.c
+++ DataTypes/genetic.c
@@ -151,24 +151,15 @@ Individual copy( Individual * target ) {
 }
 
 void swap( Individual * population, float * fitnesses, size_t i0, size_t i1) {
-    //Individual tempi = population[i0];
-    Chromosome tempi;
+    Individual tempi = population[i0];
     float tempf;
 
-    // swap sizes
-    tempf = population[i0].size;
-    population[i0].size = population[i1].size;
-    population[i1].size = tempf;
+    // swap Individuals
+    tempi = population[i0];
+    population[i0] = population[i1];
+    population[i1] = tempi;
 
-    // swap chromosomes one at a time
-    for (int i = 0; i < population[i0].size; i++) {
-        tempi = population[i0].chromosomes[i];
-        population[i0].chromosomes[i] = population[i1].chromosomes[i];
-        population[i1].chromosomes[i] = tempi;
-    }
-
-    //population[i0] = population[i1];
-    //population[i1] = tempi;
+    // swap fitnesses
     tempf = fitnesses[i0];
     fitnesses[i0] = fitnesses[i1];
     fitnesses[i1] = tempf;
